0a917d28a3d0d9a2a5a17a078f433b6db7749211
wyhbrdgh <35570503+wyhbrdgh@users.noreply.github.com>
2018-02-20 00:32:59 +0800

    test

--- src/hiworld.java
+++ src/hiworld.java
@@ -1,5 +1,6 @@
 public class hiworld {
     public static void main(String args[]){
-        System.out.println("Hi World");
+        System.out.println("Hi World" +
+                " 2018/2/20/0：31,没想到简单的提交都搞了这么多次");
     }
 }
